d0d9b20656c29ad39fdddd24483018aef909ef52
VPhoenix17 <hrishiutge2002@gmail.com>
2024-09-26 15:09:07 +0530

    'Department.Auto'

--- src/main/java/com/deloitte/demo/model/Department.java
+++ src/main/java/com/deloitte/demo/model/Department.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Department {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(name = "name")
